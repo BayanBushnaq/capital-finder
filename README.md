ca7e10f1c55e805a48b56cad7c3d5b442a8d1803
BayanBushnaq <bayanmohdali@yahoo.com>
2022-10-31 16:28:49 +0000

    feat add a function to get the ccountry of certain capital and vise versa

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # capital-finder
+
+## working example urls for deployed function
+
+- https://vercel.com/bayanbushnaq/capital-finder/api/capital-finder?country=syria
+- https://vercel.com/bayanbushnaq/capital-finder/api/capital-finder?capital=ankara 
+- https://vercel.com/bayanbushnaq/capital-finder/api/capital-finder
--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,12 +1,44 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+from urllib import parse
+import requests
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        ''' this function responsible to send a request for a REST Countries API to get a country of 
+        selected capital and vice versa '''
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dictionary = dict(query_string_list)
+
+        if 'country' in dictionary:
+            country=dictionary['country']
+            url = 'https://restcountries.com/v3.1/name/'
+            r = requests.get(url + country)
+            data = r.json()
+            capital=str(data[0]['capital'][0])
+            the_capital =f'The capital of {country} is {capital}.' 
+
+
+        elif 'capital' in dictionary:
+            capital=dictionary['capital']
+            url = 'https://restcountries.com/v3.1/capital/'
+            r = requests.get(url + capital)
+            data = r.json()
+            country=str(data[0]['name']['common'])
+            the_capital =f'{capital} is the capital of {country}.'
+
+       
+ 
+        else:
+            the_capital = "Enter the name of country or the name of capital "
+
+
+
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
         
-        self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+        self.wfile.write(the_capital.encode())
         return
